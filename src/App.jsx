125f6e2f01b9e81aa89eb07c6c47c914648278a7
Fuaad <fuaadmohamoud@gmail.com>
2025-05-11 13:34:54 -0500

    created services/login.js

--- src/App.jsx
+++ src/App.jsx
@@ -53,17 +53,7 @@ const App = () => {
     <div>
       <h2>blogs</h2>
       <Notification message={errorMessage} />
-      <form onSubmit={handleLogin}>
-        <div>
-          username
-          <input type="text" value={username} onChange={({target}) => setUsername(target.value)} />
-        </div>
-        <div>
-          password
-          <input type="password" value={password} onChange={({target}) => setPassword(target.value)} />
-        </div>
-        <button type="submit">login</button>
-      </form>
+      
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
